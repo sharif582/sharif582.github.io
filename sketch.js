47e590fdcc7aa85dee2645349912b63fc1749d9a
Sharif <sharif582@users.noreply.github.com>
2018-09-22 16:25:55 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var url = "https://crossorigin.me/https://app.spire.io/api/v2/streaks?access_token=85544b69aa3c0893e31503fb915d0b0f62d6247a6a227e5899eb601905e9c8d7";
+var data = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+function setup() {
+  noCanvas();
+  loadJSON(url, data, 'jsonp');
+print(data);
+  
+}
+
+
+  
